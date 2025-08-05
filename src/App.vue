6653560c6795a3e7b2bb824b54752582cac6ce78
张瑞卿 <1315993841@qq.com>
2025-08-05 23:26:43 +0800

    1、vite构建基础内容 2、引入devui

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <d-button>确定</d-button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import DevUI from 'vue-devui';
+import 'vue-devui/style.css';
+import '@devui-design/icons/icomoon/devui-icon.css';
+import { ThemeServiceInit, infinityTheme } from 'devui-theme';
+
+ThemeServiceInit({ infinityTheme }, 'infinityTheme');
+
+createApp(App).use(DevUI).mount('#app');
